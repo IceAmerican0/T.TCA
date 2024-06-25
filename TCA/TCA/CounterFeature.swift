f08b51e1a82f2d67929227a66c7343a02ebd6907
박성준 <SGO01016950@mobmail.shinhan.com>
2024-06-25 14:12:26 +0900

    [Essential] Side Effects

--- TCA/TCA/CounterFeature.swift
+++ TCA/TCA/CounterFeature.swift
@@ -6,29 +6,91 @@
 //
 
 import ComposableArchitecture
+import Foundation
 
 @Reducer
 struct CounterFeature {
     @ObservableState
     struct State {
         var count = 0
+        var fact: String?
+        var isLoading = false
+        var isTimerRunning = false
     }
 
     enum Action {
         case decrementButtonTapped
         case incrementButtonTapped
+        case factResponse(String)
+        case factButtonTapped
+        case timerTick
+        case toggleTimerButtonTapped
     }
+    
+    enum CancelID { case timer }
   
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
             case .decrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
                 return .none
                 
             case .incrementButtonTapped:
                 state.count += 1
+                state.fact = nil
                 return .none
+                
+            case let .factResponse(fact):
+                state.fact = fact
+                state.isLoading = false
+                return .none
+                
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+                
+                return .run { [count = state.count] send in
+                    // ✅ Do async work in here, and send actions back into the system.
+                    let (data, _) = try await URLSession.shared
+                        .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(fact))
+                }
+                
+                /*
+                 // 🛑 'async' call in a function that does not support concurrency
+                 // 🛑 Errors thrown from here are not handled
+                 
+                let (data, _) = try await URLSession.shared
+                    .data(from: URL(string: "http://numbersapi.com/\(state.count)")!)
+                
+                state.fact = String(decoding: data, as: UTF8.self)
+                state.isLoading = false
+                
+                return .none
+                */
+                
+            case .timerTick:
+                state.count += 1
+                state.fact = nil
+                return .none
+                
+            case .toggleTimerButtonTapped:
+                state.isTimerRunning.toggle()
+                if state.isTimerRunning {
+                    return .run { send in
+                        while true {
+                            try await Task.sleep(for: .seconds(1))
+                            await send(.timerTick)
+                        }
+                    }
+                    .cancellable(id: CancelID.timer)
+                } else {
+                    return .cancel(id: CancelID.timer)
+                }
             }
         }
     }
--- TCA/TCA/CounterView.swift
+++ TCA/TCA/CounterView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  CounterView.swift
 //  TCA
 //
 //  Created by Khai on 6/25/24.
@@ -34,6 +34,30 @@ struct CounterView: View {
                 .padding()
                 .background(Color.black.opacity(0.1))
                 .cornerRadius(10)
+            }
+            Button(store.isTimerRunning ? "Stop timer" : "Start timer") {
+                store.send(.toggleTimerButtonTapped)
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
+            
+            Button("Fact") {
+                store.send(.factButtonTapped)
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
+            
+            if store.isLoading {
+                ProgressView()
+            } else if let fact = store.fact {
+                Text(fact)
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .padding()
             }
         }
     }
