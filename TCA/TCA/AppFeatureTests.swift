3416b2b7c951e5b1f1020098b549357a3b22b327
Khai <starsjun@naver.com>
2024-06-26 16:38:08 +0900

    [Essential] Add AppFeature: TabView

---
+++ TCA/TCA/AppFeatureTests.swift
@@ -0,0 +1,24 @@
+//
+//  AppFeatureTests.swift
+//  TCA
+//
+//  Created by Khai on 6/26/24.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import TCA
+
+@MainActor
+final class AppFeatureTests: XCTestCase {
+    func testIncrementInFirstTab() async {
+        let store = TestStore(initialState: AppFeature.State()) {
+            AppFeature()
+        }
+        
+        await store.send(\.tab1.incrementButtonTapped) {
+            $0.tab1.count = 1
+        }
+    }
+}
--- TCA/TCA/TCAApp.swift
+++ TCA/TCA/TCAApp.swift
@@ -10,14 +10,14 @@ import ComposableArchitecture
 
 @main
 struct TCAApp: App {
-    static let store = Store(initialState: CounterFeature.State()) {
-        CounterFeature()
+    static let store = Store(initialState: AppFeature.State()) {
+        AppFeature()
         ._printChanges()
     }
   
     var body: some Scene {
         WindowGroup {
-            CounterView(store: TCAApp.store)
+            AppView(store: TCAApp.store)
         }
     }
 }
