63d317e42b76f09fdd3664e3fb22b6a653235a55
Khai <starsjun@naver.com>
2024-06-27 16:09:48 +0900

    [Navigation] ContactFeature & Fixed Tests

--- TCA/TCA/AppFeature.swift
+++ TCA/TCA/AppFeature.swift
@@ -8,24 +8,6 @@
 import ComposableArchitecture
 import SwiftUI
 
-struct AppView: View {
-    let store: StoreOf<AppFeature>
-    
-    var body: some View {
-        TabView {
-            CounterView(store: store.scope(state: \.tab1, action: \.tab1))
-                .tabItem {
-                    Text("Counter 1")
-                }
-            
-            CounterView(store: store.scope(state: \.tab2, action: \.tab2))
-                .tabItem {
-                    Text("Counter 2")
-                }
-        }
-    }
-}
-
 @Reducer
 struct AppFeature {
     struct State: Equatable {
@@ -45,6 +27,24 @@ struct AppFeature {
         }
         Reduce { state, action in
             return .none
+        }
+    }
+}
+
+struct AppView: View {
+    let store: StoreOf<AppFeature>
+    
+    var body: some View {
+        TabView {
+            CounterView(store: store.scope(state: \.tab1, action: \.tab1))
+                .tabItem {
+                    Text("Counter 1")
+                }
+            
+            CounterView(store: store.scope(state: \.tab2, action: \.tab2))
+                .tabItem {
+                    Text("Counter 2")
+                }
         }
     }
 }
