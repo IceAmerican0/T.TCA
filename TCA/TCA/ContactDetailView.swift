e1ea326dfa3b49342ac2270457a2ecbf1d19f585
Khai <starsjun@naver.com>
2024-06-28 16:40:42 +0900

    [Navigation] Stacks

---
+++ TCA/TCA/ContactDetailView.swift
@@ -0,0 +1,37 @@
+//
+//  ContactDetailView.swift
+//  TCA
+//
+//  Created by Khai on 6/28/24.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct ContactDetailView: View {
+    @Bindable var store: StoreOf<ContactDetailFeature>
+    
+    var body: some View {
+        Form {
+            Button("Delete") {
+                store.send(.deleteButtonTapped)
+            }
+        }
+        .navigationTitle(Text(store.contact.name))
+        .alert($store.scope(state: \.alert, action: \.alert))
+    }
+}
+
+#Preview {
+    NavigationStack {
+        ContactDetailView(
+            store: Store(
+                initialState: ContactDetailFeature.State(
+                    contact: Contact(id: UUID(), name: "Blob")
+                )
+            ) {
+                ContactDetailFeature()
+            }
+        )
+    }
+}
